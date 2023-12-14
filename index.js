ad7090ce8c0d70537aa33ff419708d8e41d68437
Willem Aaron G. Pacardo <iamwillempacardo@gmail.com>
2023-12-15 01:41:25 +0800

    fixed images

--- index.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Code to run after the document is fully loaded
-    alert("Welcome to PokeMart! Explore our collection of Pokémon cards.");
-});
